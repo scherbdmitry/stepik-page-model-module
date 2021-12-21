15e023a0d7c75367b03f52d81174bf1ab623cf92
scherbdmitry <scherbdmitry@gmail.com>
2021-12-21 14:42:54 +0300

    update code

--- test_main_page.py
+++ test_main_page.py
@@ -8,9 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
 
 
-
-
-
     
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
